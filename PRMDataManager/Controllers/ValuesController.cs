8805721ca9aad74c95bfaa57a6a7c28598d6eec3
Prescott Sun <sunprescott@gmail.com>
2020-06-01 16:50:16 -0400

    Add Swagger Documentation to API
    Adds Swashbuckle/Swagger to the API for documentation and testing.

--- PRMDataManager/Controllers/ValuesController.cs
+++ PRMDataManager/Controllers/ValuesController.cs
@@ -1,10 +1,6 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
-using Microsoft.AspNet.Identity;
 
 namespace PRMDataManager.Controllers
 {
